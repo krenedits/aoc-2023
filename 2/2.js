e789f35bf6b2ce9ac6679ca150bc425afbd674bc
Krenedits Ferenc <fefe.kre@gmail.com>
2023-12-04 10:18:16 +0100

    added first day from another computer

--- 2/2.js
+++ 2/2.js
@@ -1,51 +1,50 @@
-const fs = require('fs');
+const { getLines } = require("../common");
 
-const data = fs.readFileSync('input_silinfo.txt', 'utf8');
-const lines = data.split('\n');
+const lines = getLines("input.txt");
 
 const bag = {
-    red: 12,
-    green: 13,
-    blue: 14,
+  red: 12,
+  green: 13,
+  blue: 14,
 };
 
 let sum = 0;
 
 const games = lines.reduce((acc, line) => {
-    if (line === '') {
-        return acc;
-    }
+  if (line === "") {
+    return acc;
+  }
 
-    const game = line.match(/\d+/g)[0];
-    const sets = line.split(': ')[1].split('; ');
-    let maximum = {
-        green: 0,
-        blue: 0,
-        red: 0,
-    };
+  const game = line.match(/\d+/g)[0];
+  const sets = line.split(": ")[1].split("; ");
+  let maximum = {
+    green: 0,
+    blue: 0,
+    red: 0,
+  };
 
-    sets.forEach((set) => {
-        const cubes = set.split(', ');
-        cubes.forEach((cube) => {
-            const [count, color] = cube.split(' ');
+  sets.forEach((set) => {
+    const cubes = set.split(", ");
+    cubes.forEach((cube) => {
+      const [count, color] = cube.split(" ");
 
-            if (+count > maximum[color]) {
-                maximum[color] = +count;
-            }
-        });
+      if (+count > maximum[color]) {
+        maximum[color] = +count;
+      }
     });
+  });
 
-    const possible = sets.every((set) => {
-        const cubes = set.split(', ');
-        return cubes.every((cube) => {
-            const [count, color] = cube.split(' ');
+  const possible = sets.every((set) => {
+    const cubes = set.split(", ");
+    return cubes.every((cube) => {
+      const [count, color] = cube.split(" ");
 
-            return bag[color] >= count;
-        });
+      return bag[color] >= count;
     });
-    sum += Object.values(maximum).reduce((acc, val) => acc * val, 1);
+  });
+  sum += Object.values(maximum).reduce((acc, val) => acc * val, 1);
 
-    return acc + +(possible ? game : 0);
+  return acc + +(possible ? game : 0);
 }, 0);
 
 console.log(games);
